a95dd01bb8fbd7244926ba989e9efd1b30f31e09
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-11-11 09:39:56 +0000

    meshiterro

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,22 +1,24 @@
 class PostImage < ApplicationRecord
   
-   belongs_to :user
-   has_one_attached :image
-   has_many :post_comments, dependent: :destroy
-   has_many :favorites, dependent: :destroy
-  
+  belongs_to :user
+  has_one_attached :image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+   
+  validates :shop_name, presence: true
+  validates :image, presence: true
    
    
-   def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    def favorited_by?(user)
+  def get_image
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  def favorited_by?(user)
     favorites.exists?(user_id: user.id)
-    end
+  end
     
     
     image
-   end
+  end
 end
